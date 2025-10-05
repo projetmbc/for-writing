a91c9fd8e4edd1fd12ff6fc6336c33995354aedb
projetmbc <projetmbc@gmail.com>
2025-10-05 15:19:41 +0200

    [palcol]  First steps #18 - luadraw support [human comments added in extend.py file]

--- palcol/contrib/palettes/luadraw/extend.py
+++ palcol/contrib/palettes/luadraw/extend.py
@@ -14,19 +14,32 @@ import re
 
 ###
 # prototype::
-#     code : GGG
+#     code : a \std \luadraw \def of a palette named lua::''PALETTE''
+#            (see the fake example below).
 #
-#     :return: GGG
+#     :return: a list of lists of floats belonging to `[0, 1]` that
+#              will be used to produce the "universal" \json version
+#              of the palettes.
+#
+#
+# A \std \luadraw palette looks like this.
+#
+# lua::
+#     PALETTE = {
+#       {0.502, 0.502, 0.502},
+#       {0.4392, 0.502, 0.5647},
+#       ...
+#     }
 ###
-def parse(code: str) -> list[[int, int, int]]:
+def parse(code: str) -> list[[float, float, float]]:
     match = re.search(
         r'PALETTE\s*=\s*{(.*)}',
         code,
-        re.S  # dot matches all
+        re.S  # The dot operator matches all.
     )
 
     if not match:
-        raise ValueError("No PALETTE table found.")
+        raise ValueError("No table PALETTE found.")
 
     palette = match.group(1)
 
@@ -37,7 +50,7 @@ def parse(code: str) -> list[[int, int, int]]:
         palette = palette.replace(old, new)
 
     palette = f'[{palette.strip()}]'
-    palette = ast.literal_eval(palette)
+    palette = ast.literal_eval(palette)  # Safe evaluation.
 
     return palette
 
@@ -48,24 +61,27 @@ def parse(code: str) -> list[[int, int, int]]:
 
 ###
 # prototype::
-#     name : GGG
-#     data : GGG
+#     name : name of the palette without the prefix ''pal'' which is
+#            specific to \luadraw.
+#     data : a list of lists of floats that comes from  "universal"
+#            \json version of the palettes.
 #
-#     :return: GGG
+#     :return: \std \luadraw \def of the palette whose name is
+#              automatically prefixed with lua::''pal''.
 ###
-def write_new_palette(
+def build_code(
     name: str,
     data: list[list[float, float, float]]
 ) -> str:
     luadraw_code = [f"pal{name} = {{"]
 
+    indent = " "*4
+
     for r, g, b in std_data:
-        luadraw_code.append(f"    {{{r}, {g}, {b}}},")
+        luadraw_code.append(f"{indent}{{{r}, {g}, {b}}},")
 
     luadraw_code[-1] = luadraw_code[-1][:-1]
-
     luadraw_code.append("}\n")
-
     luadraw_code = '\n'.join(luadraw_code)
 
     return luadraw_code
@@ -76,8 +92,8 @@ def write_new_palette(
 # ---------------- #
 
 if __name__ == "__main__":
-# Data we have to parse.
-    PALETTE_CODE = r"""
+# Code to parse.
+    code = r"""
 PALETTE = {
   {0.502, 0.502, 0.502},
   {0.4392, 0.502, 0.5647},
@@ -91,13 +107,17 @@ PALETTE = {
 
     from rich import print
 
-    std_data  = parse(PALETTE_CODE)
+    print_section = lambda t: print(f'\n--- {t} --\n')
 
-    print(std_data )
-    print('---')
+    std_data  = parse(code)
+
+    print_section('STD DATA (JSON)')
+    print(std_data)
+
+    print_section('BACK TO CODE')
     print(
-        write_new_palette(
-          "TEST",
-          std_data
-        )
+        build_code("TEST", std_data)
     )
+
+    print_section('INITIAL CODE')
+    print(code.strip())
