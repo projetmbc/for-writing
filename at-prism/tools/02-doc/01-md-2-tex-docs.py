30ac854da7646160cb8903f6918f17b8057d1817
projetmbc <projetmbc@gmail.com>
2025-10-20 13:55:25 +0200

    [atprism] Working on English doc #15

--- at-prism/tools/02-doc/01-md-2-tex-docs.py
+++ at-prism/tools/02-doc/01-md-2-tex-docs.py
@@ -15,26 +15,54 @@ from json import load as json_load
 # -- CONSTANTS -- #
 # --------------- #
 
-THIS_DIR     = Path(__file__).parent
-PROJECT_DIR  = THIS_DIR.parent.parent
-PRODUCTS_DIR = PROJECT_DIR / "products"
-CONTRIB_DIR  = PROJECT_DIR / "contrib"
-
-CONTRIB_PRODUCTS   = CONTRIB_DIR / "products"
-CONTRIB_EN_DOC_DIR = (
+THIS_DIR        = Path(__file__).parent
+PROJECT_DIR     = THIS_DIR.parent.parent
+PRODUCTS_DIR    = PROJECT_DIR / "products"
+CONTRIB_DIR     = PROJECT_DIR / "contrib"
+MAIN_README_DIR = PROJECT_DIR / "readme"
+PROD_README_DIR = MAIN_README_DIR / "products"
+TEX_EN_DOC_DIR  = (
     CONTRIB_DIR / "translate" / "en"
                 / "doc" / "manual" / "products"
 )
 
-PAL_JSON_FILE = PRODUCTS_DIR / "palettes.json"
+MD_FILES_TO_CONVERT = [
+    MAIN_README_DIR / "products.md"
+]
+
+MD_FILES_TO_CONVERT += [
+    f
+    for f in PROD_README_DIR.glob("*.md")
+]
 
 
-with PAL_JSON_FILE.open(mode = "r") as f:
-    ALL_PALETTES = json_load(f)
+# --------------- #
+# -- CONSTANTS -- #
+# --------------- #
 
-
-
-
-
+logging.info(
+    "Updating English doc (product sections)."
+)
 
 converter = MdToLatexConverter()
+
+relENdocdir = TEX_EN_DOC_DIR.relative_to(PROJECT_DIR)
+
+for mdfile in MD_FILES_TO_CONVERT:
+    relpath_md  = mdfile.relative_to(PROJECT_DIR)
+
+    relpath_tex = (
+        "json"
+        if mdfile.stem == "products" else
+        mdfile.stem
+    )
+
+    relpath_tex = f"{relpath_tex}.tex"
+    relpath_tex = relENdocdir / f"{relpath_tex}"
+
+    logging.info(
+        msg_creation_update(
+            context = f"From '{relpath_md}' to '{relpath_tex}' TeX",
+            upper = False
+        )
+    )
