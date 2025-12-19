3024444ee7200a57efd427b42c22cca3e1acaaa4
projetmbc <projetmbc@gmail.com>
2025-12-19 12:03:21 +0100

    [aprism] Working on Lua test of the palette API (extract and co) #1

---
+++ @prism/contrib/products/lua/dev/luadraw/luadraw-pal-def.lua
@@ -0,0 +1,18 @@
+------
+-- this::
+--     author = First Name, Last Name
+------
+
+-- Dev comments?
+
+PALETTE = {
+  LimeGreen,
+  Yellow,
+  LightYellow,
+  LimeGreen,
+  Yellow,
+  LightYellow,
+  LimeGreen,
+  Yellow,
+  LightYellow
+}
