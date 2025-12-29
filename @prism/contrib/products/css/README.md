9e0836d6aa922e2af1f3e73b3923c6a8bf9ea872
projetmbc <projetmbc@gmail.com>
2025-12-29 19:14:47 +0100

    [aprism] CSS implementation #1

---
+++ @prism/contrib/products/css/README.md
@@ -0,0 +1,20 @@
+<!----------------------------------------------------------------
+  -- File created by the ''multimd'' project, version 1.0.0.    --
+  --                                                            --
+  -- ''multimd'', soon to be available on PyPI, is developed at --
+  -- https://github.com/bc-tools/for-dev/tree/main/multimd      --
+  ---------------------------------------------------------------->
+
+
+ONE-TECHNO
+==========
+
+Use a ONE-TECHNO palette
+------------------------
+
+TODO
+
+Create a palette using ONE-TECHNO
+---------------------------------
+
+TODO
---
+++ @prism/contrib/products/css/extend.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+# -------------------- #
+# -- IMPORT ALLOWED -- #
+# -------------------- #
+
+from typing import TypeAlias
+
+import ast
+import re
+
+
+# ------------ #
+# -- TYPING -- #
+# ------------ #
+
+RGBCols    :TypeAlias = [float, float, float]
+PaletteCols:TypeAlias = list[RGBCols]
+
+
+# -------------------- #
+# -- EXTRACT COLORS -- #
+# -------------------- #
+
+###
+# prototype::
+#     code : a definition of a palette with the technology chosen.
+#
+#     :return: a list of lists of 3 floats belonging to `[0, 1]` that
+#              will be used to produce the "universal" \json version
+#              of the palette.
+###
+def parse(code: str) -> PaletteCols:
+    ...
+
+
+# ---------------------- #
+# -- BUILD FINAL CODE -- #
+# ---------------------- #
+
+PALETTES_FILE_NAME = "..."
+
+###
+# prototype::
+#     credits  : the credits to the ''@prism'' project that should
+#                be added as a comment at the beginning of the final
+#                product code.
+#     palettes : the ''Python'' dictionnary of all the palettes.
+#
+#     :return: the code of the final product with all the palettes
+#              ready to be used.
+#
+#
+# warning::
+#     Except if it is totally impossible, the code build must offer
+#     the ability to access a palette via the string name of the
+#     variable associated with it, and also to propose ways to
+#     transform palettes (extraction, shift and reverse order).
+###
+def build_code(
+    credits : str,
+    palettes: dict[str, PaletteCols]
+) -> str:
+    ...
+
+
+# ---------------- #
+# -- LOCAL TEST -- #
+# ---------------- #
+
+if __name__ == "__main__":
+    ...
