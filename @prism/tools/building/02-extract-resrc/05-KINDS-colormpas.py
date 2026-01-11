e2d0719ff4d47625a52ab3c47730732fb1222f80
projetmbc <projetmbc@gmail.com>
2026-01-11 15:32:21 +0100

    [aprism] New tools #33 - Adding kinds #4

--- @prism/tools/building/02-extract-resrc/05-KINDS-colormpas.py
+++ @prism/tools/building/02-extract-resrc/05-KINDS-colormpas.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# -- DEBUG - ON -- #
+from rich import print
+# -- DEBUG - OFF -- #
+
 # ---------------------------- #
 # -- IMPORT CBUTILS - START -- #
 
@@ -18,96 +22,20 @@ from cbutils      import *
 # -------------------------- #
 
 
-import re
-import yaml
 
-# Ta variable contenant le texte Markdown
-markdown_text = """
----
- layout: default
- title: Qualitative Schemes
- nav_order: 4
- permalink: /docs/Qualitative
----
+THIS_RESRC = TAG_COLORMAPS
 
-# Qualitative Schemes
-{: .no_toc }
 
-## Table of contents
-{: .no_toc .text-delta }
+PROJ_DIR = THIS_DIR
 
-1. TOC
-{:toc}
+while (PROJ_DIR.name != TAG_APRISM):
+    PROJ_DIR = PROJ_DIR.parent
 
----
 
-## Cartocolors
+THIS_RESRC_DIR = PROJ_DIR / TAG_RESOURCES / get_stdname(THIS_RESRC)
 
-| Name        | Colormap    | Code       | Levels     |
-| ----------- | ----------- | -----------| -----------|
-| antique| ![antique](/colormaps/assets/images/cartocolors/antique.png) | ```cmaps.antique``` | 11|
-| bold| ![bold](/colormaps/assets/images/cartocolors/bold.png) | ```cmaps.bold``` | 11|
-| pastel| ![pastel](/colormaps/assets/images/cartocolors/pastel.png) | ```cmaps.pastel``` | 11|
-| **prism**| ![prism](/colormaps/assets/images/cartocolors/prism.png) | ```cmaps.prism``` | 256|
-| safe| ![safe](/colormaps/assets/images/cartocolors/safe.png) | ```cmaps.safe``` | 11|
-| vivid| ![vivid](/colormaps/assets/images/cartocolors/vivid.png) | ```cmaps.vivid``` | 11|
 
-## Colorbrewer
 
-| Name        | Colormap    | Code       | Levels     |
-| ----------- | ----------- | -----------| -----------|
-| accent| ![accent](/colormaps/assets/images/colorbrewer/accent.png) | ```cmaps.accent``` | 8|
-| dark2| ![dark2](/colormaps/assets/images/colorbrewer/dark2.png) | ```cmaps.dark2``` | 8|
-| paired| ![paired](/colormaps/assets/images/colorbrewer/paired.png) | ```cmaps.paired``` | 9|
-| pastel1| ![pastel1](/colormaps/assets/images/colorbrewer/pastel1.png) | ```cmaps.pastel1``` | 9|
-| pastel2| ![pastel2](/colormaps/assets/images/colorbrewer/pastel2.png) | ```cmaps.pastel2``` | 8|
-| set1| ![set1](/colormaps/assets/images/colorbrewer/set1.png) | ```cmaps.set1``` | 9|
-| set2| ![set2](/colormaps/assets/images/colorbrewer/set2.png) | ```cmaps.set2``` | 8|
-| set3| ![set3](/colormaps/assets/images/colorbrewer/set3.png) |
-
-## SciViz
-
-| Name        | Colormap    | Code       | Levels     |
-| ----------- | ----------- | -----------| -----------|
-| discrete_Bg| ![discrete_Bg](/colormaps/assets/images/sciviz/discrete_Bg.png) | ```cmaps.discrete_Bg``` | 8|
-| discrete_Bo| ![discrete_Bo](/colormaps/assets/images/sciviz/discrete_Bo.png) | ```cmaps.discrete_Bo``` | 8|
-| discrete_autumn| ![discrete_autumn](/colormaps/assets/images/sciviz/discrete_autumn.png) | ```cmaps.discrete_autumn``` | 10|
-| discrete_dark| ![discrete_dark](/colormaps/assets/images/sciviz/discrete_dark.png) | ```cmaps.discrete_dark``` | 10|
-| discrete_light_aut| ![discrete_light_aut](/colormaps/assets/images/sciviz/discrete_light_aut.png) | ```cmaps.discrete_light_aut``` | 8|
-| discrete_muted| ![discrete_muted](/colormaps/assets/images/sciviz/discrete_muted.png) | ```cmaps.discrete_muted``` | 14|
-| discrete_vaneyck| ![discrete_vaneyck](/colormaps/assets/images/sciviz/discrete_vaneyck.png) | ```cmaps.discrete_vaneyck``` | 8|
-
-## Tableau
-
-| Name        | Colormap    | Code       | Levels     |
-| ----------- | ----------- | -----------| -----------|
-| bluered_12| ![bluered_12](/colormaps/assets/images/tableau/bluered_12.png) | ```cmaps.bluered_12``` | 12|
-| bluered_6| ![bluered_6](/colormaps/assets/images/tableau/bluered_6.png) | ```cmaps.bluered_6``` | 6|
-| colorblind_10| ![colorblind_10](/colormaps/assets/images/tableau/colorblind_10.png) | ```cmaps.colorblind_10``` | 10|
-| gray_5| ![gray_5](/colormaps/assets/images/tableau/gray_5.png) | ```cmaps.gray_5``` | 5|
-| greenorange_12| ![greenorange_12](/colormaps/assets/images/tableau/greenorange_12.png) | ```cmaps.greenorange_12``` | 12|
-| greenorange_6| ![greenorange_6](/colormaps/assets/images/tableau/greenorange_6.png) | ```cmaps.greenorange_6``` | 6|
-| purplegray_12| ![purplegray_12](/colormaps/assets/images/tableau/purplegray_12.png) | ```cmaps.purplegray_12``` | 12|
-| purplegray_6| ![purplegray_6](/colormaps/assets/images/tableau/purplegray_6.png) | ```cmaps.purplegray_6``` | 6|
-| tableau_10| ![tableau_10](/colormaps/assets/images/tableau/tableau_10.png) | ```cmaps.tableau_10``` | 10|
-| tableau_20| ![tableau_20](/colormaps/assets/images/tableau/tableau_20.png) | ```cmaps.tableau_20``` | 20|
-| tableaulight_10| ![tableaulight_10](/colormaps/assets/images/tableau/tableaulight_10.png) | ```cmaps.tableaulight_10``` | 10|
-| tableaumedium_10| ![tableaumedium_10](/colormaps/assets/images/tableau/tableaumedium_10.png) | ```cmaps.tableaumedium_10``` | 10|
-| trafficlight_9| ![trafficlight_9](/colormaps/assets/images/tableau/trafficlight_9.png) | ```cmaps.trafficlight_9``` | 9|
-
-## ColorCET
-
-| Name        | Colormap    | Code       | Levels     |
-| ----------- | ----------- | -----------| -----------|
-| cet_g_bw| ![cet_g_bw](/colormaps/assets/images/colorcet/cet_g_bw.png) | ```cmaps.cet_g_bw``` | 256|
-| cet_g_bw_minc| ![cet_g_bw_minc](/colormaps/assets/images/colorcet/cet_g_bw_minc.png) | ```cmaps.cet_g_bw_minc``` | 256|
-| cet_g_bw_minc1| ![cet_g_bw_minc1](/colormaps/assets/images/colorcet/cet_g_bw_minc1.png) | ```cmaps.cet_g_bw_minc1``` | 256|
-| cet_g_bw_minc_maxl| ![cet_g_bw_minc_maxl](/colormaps/assets/images/colorcet/cet_g_bw_minc_maxl.png) | ```cmaps.cet_g_bw_minc_maxl``` | 256|
-| cet_g_bw_minc_minl| ![cet_g_bw_minc_minl](/colormaps/assets/images/colorcet/cet_g_bw_minc_minl.png) | ```cmaps.cet_g_bw_minc_minl``` | 256|
-| cet_g_category10| ![cet_g_category10](/colormaps/assets/images/colorcet/cet_g_category10.png) | ```cmaps.cet_g_category10``` | 256|
-| cet_g_hv| ![cet_g_hv](/colormaps/assets/images/colorcet/cet_g_hv.png) | ```cmaps.cet_g_hv``` | 256|
-
-"""
 
 def parse_markdown_palettes(text):
     # 1. Extraire le type de schéma (Titre #)
@@ -150,10 +78,6 @@ def parse_markdown_palettes(text):
 
 # --- Exécution et affichage ---
 
-data = parse_markdown_palettes(markdown_text)
-
-class IndentDumper(yaml.SafeDumper):
-    def increase_indent(self, flow=False, indentless=False):
-        return super(IndentDumper, self).increase_indent(flow, False)
-
-print(yaml.dump(data, Dumper=IndentDumper, indent=4, sort_keys=True))
+for mdpath in THIS_RESRC_DIR.glob('docs/*.md'):
+    print()
+    print(parse_markdown_palettes(mdpath.read_text()))
