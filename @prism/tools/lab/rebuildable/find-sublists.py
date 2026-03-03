3c59f3254fbf9ce149554f9fec15e28cc3c46060
projetmbc <projetmbc@gmail.com>
2026-03-03 20:12:41 +0100

    [aprism] Working on data extraction for sublist palettes #8

--- @prism/tools/lab/rebuildable/find-sublists.py
+++ @prism/tools/lab/rebuildable/find-sublists.py
@@ -12,13 +12,3 @@ PROJ_DIR = THIS_DIR.parent
 
 while(PROJ_DIR.name != '@prism'):
     PROJ_DIR = PROJ_DIR.parent
-
-IGNORED_YAML  = PROJ_DIR / 'tools' / 'building' / 'AUDIT' / 'IGNORED.yaml'
-
-IGNORED = safe_load(IGNORED_YAML.read_text())
-
-NORMALIZED_IGNORED = defaultdict(dict)
-
-for src, data in IGNORED.items():
-    for palname, infos in data.items():
-        print(f"{src=} , {palname=} , {infos=}")
